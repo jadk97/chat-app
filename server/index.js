8cc1902d8aad3f52daed9383710a42b264405573
Jad K <jad_dis@hotmail.com>
2020-01-09 04:06:12 -0500

    cleaned up some of the server side code and implemented the basis for a userlist on the frontend

--- server/index.js
+++ server/index.js
@@ -23,22 +23,29 @@ io.on("connection", (socket) => {
       return callback(error);
     }
 
-    socket.emit('message', { user: `admin`, text: `${user.name}, welcome to the room ${user.room}` })
-    socket.broadcast.to(user.room).emit('message', { user: `admin`, text: `${user.name} has joined.`});
+    socket.emit('message', { user: `admin`, text: `Hi, ${user.name}. Welcome to room '${user.room}'.` })
+    socket.broadcast.to(user.room).emit('message', { user: `admin`, text: `${user.name} has joined the room.`});
     
     
     socket.join(user.room);
+
+    io.to(user.room).emit('roomData', {room: user.room, users:getUsersInRoom(user.room)})
+    callback();
   });
 
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
     io.to(user.room).emit('message', { user: user.name, text: message});
-
+    io.to(user.room).emit('message', { room: user.room, users:getUsersInRoom(user.room)});
     callback();
   });
 
   socket.on("disconnect", () => {
-    console.log("A user has disconnected.");
+    const user = removeUser(socket.id);
+
+    if(user){
+      io.to(user.room).emit('message', {user: "admin", text: `${user.name} has left the room.`});
+    }
   });
 });
 app.use(router);
