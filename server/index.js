c555ac0f5961ce79bc821a3f5003d3555bc4a0dc
Jad K <jad_dis@hotmail.com>
2020-01-09 01:16:51 -0500

    created router file and setup sockets on the server side

--- server/index.js
+++ server/index.js
@@ -2,8 +2,21 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 const PORT = process.env.PORT || 5000;
+
+const router = require("./router");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on("connection", (socket) => {
+  console.log("We have a new connection.");
+
+  socket.on("disconnect", () => {
+    console.log("A user has disconnected.");
+  });
+});
+app.use(router);
+
 server.listen(PORT, () => console.log(`Server is now running on ${PORT}`));
+
