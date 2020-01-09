4cd1565bc1eabb2cd01b58a923e86ce852676f2c
Jad K <jad_dis@hotmail.com>
2020-01-09 03:09:10 -0500

    created InfoBar component and added some styling

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
 import io from "socket.io-client";
-
+import InfoBar from "../InfoBar/InfoBar";
+import "./Chat.css";
 let socket;
 
 const Chat = ({ location }) => {
@@ -46,13 +47,9 @@ const Chat = ({ location }) => {
 
   console.log(message, messages);
   return (
-    <div className="outercontainer">
+    <div className="outerContainer">
       <div className="container">
-        <input
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-          onKeyPress={event => event.key === "Enter" ? sendMessage(event) : null}
-        />
+      <InfoBar room = {room} />
       </div>
     </div>
   )
